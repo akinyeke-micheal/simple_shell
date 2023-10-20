cbb965d3f61cdc05bf51c950e92c4617a6d5a83b
AKINYEKE MICHEAL OLAKUNLE <106710581+akinyeke-micheal@users.noreply.github.com>
2023-10-20 01:39:49 +0100

    Update alternator.c

--- alternator.c
+++ alternator.c
@@ -1,17 +1,4 @@
 #include "shell.h"
-
-/**
- * clear_info - initializes info_t struct
- * @info: struct address
- */
-void clear_info(info_t *info)
-{
-	info->arg = NULL;
-	info->argv = NULL;
-	info->path = NULL;
-	info->argc = 0;
-}
-
 /**
  * set_info - initializes info_t struct
  * @info: struct address
