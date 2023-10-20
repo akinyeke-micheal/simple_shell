81b75c66e8b1be65d2cf9259c10cf8f415b0a019
AKINYEKE MICHEAL OLAKUNLE <106710581+akinyeke-micheal@users.noreply.github.com>
2023-10-20 03:11:13 +0100

    Update and rename caij.c to HANDLER_B.c

--- HANDLER_B.c
+++ HANDLER_B.c
@@ -1,12 +1,5 @@
 #include "shell.h"
 
-/**
- * is_chain - test if current char in buffer is chain delimeter
- * @info: parameter struct
- * @buf: char buffer
- * @p: address of current position in buf
- * Return: 1 if chain delimeter, 0 otherwise
- */
 
 int is_chain(info_t *info, char *buf, size_t *p)
 {
@@ -76,7 +69,7 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
  */
 
 int replace_alias(info_t *info)
-{
+{r
 	int i;
 	list_t *node;
 	char *p;
